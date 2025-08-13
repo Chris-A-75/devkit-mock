80300d2b1ed3efaa32021de7195d0331b9407e01
Chris-A-75 <chrisaccaoui@protonmail.com>
2025-08-13 12:38:05 +0300

    -Made it more responsive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
-import { Button, Typography, Container,Box } from "@mui/material";
+import { Button, Typography, Container, Box } from "@mui/material";
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import image from './image.png';
 
 function App() {
   return (
-     <Box sx={{ minHeight: "100vh", backgroundColor: "#e0f0ff", py: 3 }}>
-    <Container sx={{ backgroundColor: "#fafcfb", borderRadius: 5, mt:10 }}>
+     <Box sx={{ minHeight: "100vh", backgroundColor: "#e0f0ff", pt: "5%" }}>
+    <Container sx={{ backgroundColor: "#fafcfb", borderRadius: 5, mt:2 }}>
         <Box sx={{pt:2,ml:5, display:"flex"}}>
         <Button sx={{color: 'inherit', textTransform: 'none'}} disableRipple><Typography sx={{color:"blue"}} variant="h5">Devkit.</Typography></Button>
-        <Box sx={{display:"flex", ml:70,color:"grey"}}>
+        <Box sx={{display:"flex", flexWrap:"wrap", ml:"50%",color:"grey"}}>
             <Button sx={{color:"grey"}}>Account</Button>
             <Button sx={{color:"grey"}}>About</Button>
             <Button sx={{color:"grey"}}>Service</Button>
             <Button sx={{color:"grey"}}>Contact</Button>
         </Box>
         </Box><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-        <Box sx={{display:"flex"}}>
-        <Box sx={{mr:20, ml:15}}>
+        <Box sx={{display:"flex", flexDirection:{sm:"row", xs:"column"}, pb:"5%"}}>
+        <Box sx={{mr:"20%", ml:"10%"}}>
             <Typography variant="h4">
                 Welcome to <span style={{color:"blue"}}>Devkit.</span> Develop anything.
             </Typography>
@@ -29,7 +29,9 @@ function App() {
                 <Button variant="outlined"> Documentation </Button>
             </Box>
         </Box>
-        <img src={image} style={{width:500 ,transform: "translateY(-70px)"}}></img>
+        <Box sx={{width:{xs:"100%", sm:1000},transform: {xs:"none", sm:"translateY(-70px)"}}}>
+        <img src={image} style={{width:"100%"}}></img>
+        </Box>
         </Box>
     </Container>
     </Box>
